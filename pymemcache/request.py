d64556fc0bb9e55190a445fc1e3f7a6fbe847ef5
Eric Scrivner <eric.t.scrivner@gmail.com>
2015-05-07 16:07:40 -0700

    Add Simple Request Validation

--- pymemcache/request.py
+++ pymemcache/request.py
@@ -15,3 +15,17 @@ logger = logging.getLogger(__name__)
 
 class InvalidRequestError(errors.Error):
     """Error raised when a request is malformed or invalid"""
+
+
+def is_valid_request(command):
+    """Indicates whether or not the given command is valid.
+
+    :param command: A command
+    :type command: str or unicode
+    :rtype: bool
+    """
+    if not command:
+        return False
+
+    parts = command.split('\r\n')
+    return 'get' in parts[0].lower()
